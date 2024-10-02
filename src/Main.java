d4aa84393199ccfcc3bea06bc10a84e955b7c350
LENOVO <abdumalikovvv6@gmail.com>
2024-10-02 13:54:09 +0600

    Method1

---
+++ src/Main.java
@@ -0,0 +1,112 @@
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+//    Scanner scanner = new Scanner(System.in);
+//    int a = scanner.nextInt();
+//
+//    int d = scanner.nextInt();
+//
+//    int b = scanner.nextInt();
+//
+//
+//    System.out.println(Method(a,d,b));
+//
+//    }
+//  public static int Method(int a, int d, int b){
+//
+//      return (a+d+b)/3;
+
+//        Sancho(8, "Hello");
+//
+//
+//    }
+//
+//
+//    static void Sancho(int number, String text) {
+//        for (int i = 0; i < number; i++) {
+//
+//
+//        }
+//
+//    }
+
+//        static int maxoo(int num1, int num2,int num3) {
+//
+//        int max = num1;
+//
+//        if (num2 > max){
+//            max = num2;
+//        }
+//
+//        if (num2 > max){
+//            max = num3;
+//        }
+//
+//
+//        return max;
+
+
+//    public static int findMin(int[] array) {
+//        int min = array[0];
+//        for (int i = 1; i < array.length; i++){
+//            if (array[i] < min) {
+//                min = array[i];
+//            }
+//        }
+//        return min;
+//    }
+//    public static void Main(String[] args)  {
+//        Scanner scanner = new Scanner(System.in);
+//        int[] array = new int[10];
+//
+//
+//        System.out.println("massivdin 10 elemehtin kirgiziniz:");
+//        for (int i = 0; i < 10; i++) array[i] = Scanner.NextInt;
+//
+//        //metoddu chakyryp, en kichine elemmentti alabyz
+//            int min = findMin(array);
+//            System.out.println("en kichine element: " + min);
+//
+//         //en kichine elementtin kvadratyn tabuu
+//         int square = min * min;
+//         System.out.println("en richine elementtin rva ");
+//
+//        }
+//
+//          String[] massive={"Санжар", "Азмар", "Адилет" };
+//
+//          for (String i : massive){
+//              System.out.println(i+" is a develover ");
+//          }
+//
+
+
+
+
+
+
+
+
+
+
+
+    }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
